7eb830a50fec900cb8147cf872afcb8d19d0c638
Pradhumn <sirothiyapradhumn@gmail.com>
2025-06-01 19:28:41 +0530

    feat: Implement user login functionality and refine registration error handling

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,9 +3,8 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
-  
   try {
-     //extract user information from our request body
+    //extract user information from our request body
     const { username, password, email, role } = req.body;
 
     //check if the user is already exists in our database
@@ -13,16 +12,16 @@ const registerUser = async (req, res) => {
       $or: [{ username }, { email }],
     });
 
-    if(checkExistingUser) {
+    if (checkExistingUser) {
       return res.status(400).json({
         success: false,
-        message: "User is already exists with same email or usename."
+        message: "User is already exists with same email or usename.",
       });
     }
 
     //hash user password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword  = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     //create a new user and save in your database
     const newlyCreatedUser = new User({
@@ -34,7 +33,7 @@ const registerUser = async (req, res) => {
 
     await newlyCreatedUser.save();
 
-    if(newlyCreatedUser) {
+    if (newlyCreatedUser) {
       res.status(201).json({
         success: true,
         message: "User successfully registered",
@@ -54,8 +53,56 @@ const registerUser = async (req, res) => {
   }
 };
 
-// const loginUser = async (req, res) => {
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-// };
+    const user = await User.findOne({ username });
 
-module.exports = { registerUser,};
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: `User doesn't exists`,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials!",
+      });
+    }
+
+    //create a access token
+
+    const accessToken = jwt.sign(
+      {
+        userId: user._id,
+        username: user.username,
+        role: user.role,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "30m",
+      }
+    );
+
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      accessToken
+    })
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error!!!",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { registerUser } = require('../controllers/auth-controller');
+const { registerUser, loginUser } = require('../controllers/auth-controller');
 
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-// router.post("/login", loginUser);
+router.post("/login", loginUser);
 
 module.exports = router;
